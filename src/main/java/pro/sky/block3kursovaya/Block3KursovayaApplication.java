06cf429eb94df50013a9efba04f5f3fd800b9c77
Степан <www.niharaktera@yandex.ru>
2023-04-08 15:21:23 +0300

    создал мастер для курсовой

---
+++ src/main/java/pro/sky/block3kursovaya/Block3KursovayaApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.block3kursovaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Block3KursovayaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Block3KursovayaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/block3kursovaya/Block3KursovayaApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.block3kursovaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Block3KursovayaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
